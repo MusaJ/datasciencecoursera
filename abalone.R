58658e5b68054e9ab929639c5ed332f2d70b589a
Musa <musa.jahanghir@Live.com>
2015-04-28 10:47:32 -0700

    generated nice reports for reproducible research

--- abalone.R
+++ abalone.R
@@ -1,12 +1,15 @@
 abalone <- read.csv("abalone.csv")
 meanLength <- mean(abalone$Length)
+table(abalone$Sex)
+library(ggplot2)
+# library(pdflatex)
+plot(Length ~ Sex, data=abalone)
+qplot(x=Rings,y=Length,data=abalone)
 model <- lm(Whole.weight ~ Length + Sex, data=abalone)
 x <- 1:3
 cv <- function (x, na.rm=FALSE){
   sd(x,na.rm=na.rm)/mean(x,na.rm=na.rm)
 }
+cv(abalone$Length)
 model
 summary(model)
-table(abalone$Sex)
-plot(Length ~ Sex, data=abalone)
-cv(abalone$Length)
